712833ad1df52c68565a6bbce7f7ecf6a93865e5
kianwei96 <kianwei96@gmail.com>
2020-12-14 12:48:42 +0800

    update dir in bash files

--- ros_launch.sh
+++ ros_launch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 xterm -iconic -hold -e "roscore" &
-sleep 1; xterm -iconic -hold -e "cd ~/catkin_ws; roslaunch robot.launch" & #RosAria
-sleep 1; xterm -iconic -hold -e "cd ~/catkin_ws; roslaunch hokuyo.launch" &
-sleep 2; xterm -iconic -hold -e "cd ~/catkin_ws; rosrun robot_setup_tf tf_broadcaster" &
-sleep 2; xterm -iconic -hold -e "cd ~/catkin_ws; rosrun rviz rviz" &
-sleep 2; xterm -iconic -hold -e "cd ~/catkin_ws; rosrun map_server map_server ~/ExperimentPlatform/maptest1.yaml" &
-sleep 2; xterm -iconic -hold -e "cd ~/catkin_ws; roslaunch AMCL.launch" &
-sleep 2; xterm -iconic -hold -e "cd ~/catkin_ws; rostopic echo /amcl_pose"&
-sleep 2; xterm -iconic -hold -e "cd ~/catkin_ws; rosrun joy joy_node _autorepeat_rate:=10"&
-sleep 2; xterm -iconic -hold -e "cd ~/catkin_ws; rostopic echo joy"
+sleep 1; xterm -iconic -hold -e "cd ~/ExperimentPlatform/launch; roslaunch robot.launch" & #RosAria
+sleep 1; xterm -iconic -hold -e "cd ~/ExperimentPlatform/launch; roslaunch hokuyo.launch" &
+sleep 2; xterm -iconic -hold -e "cd ~/ExperimentPlatform/launch; rosrun robot_setup_tf tf_broadcaster" &
+sleep 2; xterm -iconic -hold -e "cd ~/ExperimentPlatform/launch; rosrun rviz rviz" &
+sleep 2; xterm -iconic -hold -e "cd ~/ExperimentPlatform/launch; rosrun map_server map_server ~/ExperimentPlatform/maptest1.yaml" &
+sleep 2; xterm -iconic -hold -e "cd ~/ExperimentPlatform/launch; roslaunch AMCL.launch" &
+sleep 2; xterm -iconic -hold -e "cd ~/ExperimentPlatform/launch; rostopic echo /amcl_pose"&
+sleep 2; xterm -iconic -hold -e "cd ~/ExperimentPlatform/launch; rosrun joy joy_node _autorepeat_rate:=10"&
+sleep 2; xterm -iconic -hold -e "cd ~/ExperimentPlatform/launch; rostopic echo joy"
 #sleep 10; xterm -hold -e "cd catkin_ws; rosrun teleop_twist_joy joy_teleop.py"
