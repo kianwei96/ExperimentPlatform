fb36e0bbe6568109f2e615a994166f987066c16f
ndhuu <danghuu.dna@gmail.com>
2021-02-23 11:02:07 +0800

    init eyelink file

---
+++ calibrate_video.py
@@ -0,0 +1,63 @@
+import eyelink
+from psychopy import visual, core
+
+def main(edffile='test.edf',
+         screen_width=800,
+         screen_height=600,
+         full_screen=True,
+         dot_duration=1.0):
+
+    # create a window
+    win = visual.Window(
+        size=(screen_width, screen_height), fullscr=full_screen, screen=0,
+        allowGUI=True, allowStencil=False,
+        monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
+        blendMode='avg', useFBO=False)
+
+    # use pixels as units
+    win.setUnits('pix')
+
+    # create a visual stimulus for the dots
+    dot = visual.Circle(win, 50.0)
+    margins = [0.1*screen_width, 0.1*screen_height]
+    xmin = margins[0]
+    xmax = screen_width - margins[0]
+    ymin = margins[1]
+    ymax = screen_height - margins[1]
+    dot_pos = [(xmin, ymin),
+               (xmin, ymax),
+               (xmax, ymax),
+               (xmax, ymin)]
+    print(dot_pos)
+    
+    # keep track of time
+    clock = core.Clock()
+
+    # establish a connection to the tracker
+    tracker = eyelink.Eyelink(win, edffile)
+
+    # start the recording
+    tracker.start_recording()
+   
+    idx = 0
+    clock.reset()
+    win.callOnFlip(lambda: tracker.send_message('dot pos: {}'.format(dot.pos)))
+    while True:
+        dot.pos = dot_pos[idx]
+        dot.draw()
+        if clock.getTime() > dot_duration:
+            idx += 1
+            clock.reset()
+            win.callOnFlip(lambda: tracker.send_message('dot pos: {}'.format(dot.pos)))
+
+        if idx >= len(dot_pos):
+            break
+            # idx=0
+        
+        win.flip()
+    
+    win.close()
+    tracker.stop_recording()
+
+if __name__ == '__main__':
+    main()
---
+++ eyelink.py
@@ -0,0 +1,46 @@
+from psychopy import event
+from pylinkwrapper import connector
+from psychopy.tools.monitorunittools import pix2deg
+import os
+
+class Eyelink():
+        def __init__(self, win, edfname):
+            self.tracker = connector.Connect(win, edfname)
+
+        def get_position(self):
+            x, y = self.tracker.get_gaze()
+            elx = pix2deg(x - self.tracker.scenter[0],
+                          self.tracker.win.monitor)
+            ely = pix2deg(-(y - self.tracker.scenter[1]),
+                          self.tracker.win.monitor)
+            return elx, ely
+        
+        def start_recording(self):
+            self.tracker.record_on(True)
+        
+        def stop_recording(self, datadir='data'):
+            self.tracker.record_off()
+            if not os.path.isdir(datadir):
+                os.mkdir(datadir)
+            self.tracker.end_experiment(os.path.join(os.getcwd(), datadir))
+        
+        def send_message(self, message):
+            self.tracker.send_message(message)
+
+
+class DummyTracker():
+    def __init__(self, win):
+        self.win = win
+        self.mouse = event.Mouse()
+    
+    def get_position(self):
+        return self.mouse.getPos()
+
+    def start_recording(self):
+        pass
+
+    def stop_recording(self):
+        pass
+
+    def send_message(self, m):
+        pass
